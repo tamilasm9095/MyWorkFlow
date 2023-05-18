3c97d2da7ea7820b4ec8d9671a1f16e8ec3a0d3e
Tamilselvan <133114564+tamilasm9095@users.noreply.github.com>
2023-05-18 10:01:49 +0530

    Delete api directory

--- api/hourwise.py
+++
@@ -1,9 +0,0 @@
-from  main import  hourwise,onlyhourwise,SendMail,yesterday_str,df,removepdf
-
-hourwise(df)
-
-
-
-
-SendMail(onlyhourwise)
-removepdf()
--- api/logger.py
+++
@@ -1,3 +0,0 @@
-import logging
-
-logging.basicConfig(filename='/home/ubuntu/democron/project.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
